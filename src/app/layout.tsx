d3d99a8d5b5b45dbe6f3a49e24ba32241c8fab7c
Ellie Frymire <efrymire@netflix.com>
2024-12-29 18:51:33 -0800

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${robotoMono.variable} antialiased `}>
         <div className="flex flex-col items-center justify-between min-h-screen pb-10">
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
